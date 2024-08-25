872bc316389b0804413caa04394ea718fab15389
easonwang <s102401002@gmail.com>
2024-08-25 22:48:32 +0800

    完成心臟病pipeline

--- HeartDisease_prediction.py
+++ HeartDisease_prediction.py
@@ -19,8 +19,10 @@ def load_data(data_output: Output[Artifact]):
     import pandas as pd
     
     # 讀取與程式碼位於同一個資料夾中的 stroke.csv
-    df_data = pd.read_csv('heart_2020_cleaned.csv')
-
+    # urls = [
+    #     "https://raw.githubusercontent.com/s102401002/kubeflowPipeline0722/main/heart_2020_cleaned.csv"
+    # ]
+    df_data = pd.read_csv("https://raw.githubusercontent.com/s102401002/kubeflowPipeline0722/main/heart_2020_cleaned.csv")
     # 移除不需要的欄位
     df_data = df_data.drop(columns=['PhysicalHealth', 'MentalHealth', 'Race' , 'GenHealth'])
 
@@ -82,81 +84,117 @@ def load_data(data_output: Output[Artifact]):
 )
 def prepare_data(
     data_input: Input[Artifact], 
-    x_train_output: Output[Artifact], x_test_output: Output[Artifact],
-    y_train_output: Output[Artifact], y_test_output: Output[Artifact]
+    X_train_output: Output[Artifact], X_test_output: Output[Artifact],
+    Y_train_output: Output[Artifact], Y_test_output: Output[Artifact],
+    X_val_output: Output[Artifact], Y_val_output: Output[Artifact]
 ):
     import pandas as pd
     from sklearn.model_selection import train_test_split
+
+    df_data = pd.read_csv(data_input.path)
 
     X = df_data.drop(labels=['HeartDisease'], axis=1)
     Y = df_data[['HeartDisease']]
     
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
     X_test, X_val, Y_test, Y_val = train_test_split(X_test, Y_test, test_size=0.5, random_state=42)
-    print('X_Training data shape:' , X_train.shape)
-    print('X_Validation data shape:' , X_val.shape)
-    print('X_Test data shape:' , X_test.shape)
-    print('Y_Training data shape:' , Y_train.shape)
-    print('Y_Validation data shape:' , Y_val.shape)
-    print('Y_Test data shape:' , Y_test.shape)
+    X_train.to_csv(X_train_output.path, index=False)
+    X_test.to_csv(X_test_output.path, index=False)
+    Y_train.to_csv(Y_train_output.path, index=False)
+    Y_test.to_csv(Y_test_output.path, index=False)
+    X_val.to_csv(X_val_output.path, index=False)
+    Y_val.to_csv(Y_val_output.path, index=False)
+
 
 @dsl.component(
     base_image='python:3.9',
-    packages_to_install=['pandas==2.2.2', 'scikit-learn==1.5.1', 'joblib==1.4.2']
+    packages_to_install=['pandas==2.2.2', 'scikit-learn==1.5.1', 'joblib==1.4.2', 'xgboost==2.0.3']
 )
-def train_model(x_train: Input[Artifact], y_train: Input[Artifact], train_model_output: Output[Artifact]):
+def train_model(
+    X_train: Input[Artifact], 
+    Y_train: Input[Artifact], 
+    X_test: Input[Artifact], 
+    Y_test: Input[Artifact], 
+    X_val: Input[Artifact], 
+    Y_val: Input[Artifact]
+) -> str:
     import pandas as pd
+    import xgboost as xgb
     from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import accuracy_score
     import joblib
     
-    x_train = pd.read_csv(x_train.path)
-    y_train = pd.read_csv(y_train.path)
+    X_train = pd.read_csv(X_train.path)
+    Y_train = pd.read_csv(Y_train.path)
+    X_test = pd.read_csv(X_test.path)
+    Y_test = pd.read_csv(Y_test.path)
+    X_val = pd.read_csv(X_val.path)
+    Y_val = pd.read_csv(Y_val.path)
     
-    model = LogisticRegression(random_state=0, max_iter=10000) # 100 times for test p.s. it is 10000 times in beginning
-    model.fit(x_train, y_train)
+    # Logistic Regression
+    print('Logistic Regression')
+    model = LogisticRegression(random_state=0, max_iter=10000)
+    model.fit(X_train, Y_train)
+    print('Training accuracy:', model.score(X_train, Y_train))
+    lr_accuracy = model.score(X_test, Y_test)
+    print('Test accuracy:', lr_accuracy)
     
-    #model_path = './diabete_prediction_model.pkl'
-    joblib.dump(model, train_model_output.path)
+    # XGBoost
+    print('XGBoost')
+    def xgboost_training():
+        dtrain = xgb.DMatrix(X_train, label=Y_train)
+        dval = xgb.DMatrix(X_val, label=Y_val)
+        dtest = xgb.DMatrix(X_test, label=Y_test)
 
-@dsl.component(
-    base_image='python:3.9',
-    packages_to_install=['pandas==2.2.2', 'scikit-learn==1.5.1', 'joblib==1.4.2']
-)
-def evaluate_model(model_path: Input[Artifact], x_test: Input[Artifact], y_test: Input[Artifact]) -> str:
-    import pandas as pd
-    import sklearn
-    import joblib
-
-    model = joblib.load(filename=model_path.path)
-
-    x_test_df = pd.read_csv(x_test.path)
-    y_test_df = pd.read_csv(y_test.path)
+        scale_pos_weight = len(Y_train[Y_train == 0]) / len(Y_train[Y_train == 1])
+        param = {
+            'max_depth': 3,
+            'eta': 0.3,
+            'objective': 'binary:logistic',
+            'eval_metric': 'logloss',
+            'scale_pos_weight': scale_pos_weight
+        }
+        evallist = [(dtrain, 'train'), (dval, 'eval')]
+        num_round = 1000
+        model = xgb.train(param, dtrain, num_round, evallist, early_stopping_rounds=10)
+        preds = model.predict(dtest)
+        predictions = [round(value) for value in preds]
+        global xgb_accuracy 
+        xgb_accuracy = accuracy_score(Y_test, predictions)
+        print('XGBoost Test accuracy:', xgb_accuracy)
+        
+        return model
     
-    accuracy = model.score(x_test_df, y_test_df)
+    xgb_model = xgboost_training()
     
-    return f'Test accuracy: {accuracy}'
+    # Save the model
+    # joblib.dump(xgb_model, model_output.path)
+    return f'Logistic Regression accuracy: {lr_accuracy}, XGBoost accuracy: {xgb_accuracy}'
 
 @dsl.pipeline(
-    name='Diabetes Prediction Pipeline',
-    description='Using kubeflow pipeline to train and evaluate a diabetes prediction model'
+    name='HeartDisease Prediction Pipeline',
+    description='Using Kubeflow pipeline to train and evaluate a HeartDisease prediction model'
 )
-def diabetes_prediction_pipeline() -> str:
+def HeartDisease_prediction_pipeline() -> str:
+    # 加載數據
     load_data_task = load_data()
 
+    # 準備數據
     prepare_data_task = prepare_data(data_input=load_data_task.outputs['data_output'])
     
+    # 訓練模型
     train_model_task = train_model(
-        x_train = prepare_data_task.outputs['x_train_output'], 
-        y_train = prepare_data_task.outputs['y_train_output']
+        X_train=prepare_data_task.outputs['X_train_output'], 
+        Y_train=prepare_data_task.outputs['Y_train_output'],
+        X_test=prepare_data_task.outputs['X_test_output'], 
+        Y_test=prepare_data_task.outputs['Y_test_output'],
+        X_val=prepare_data_task.outputs['X_val_output'],
+        Y_val=prepare_data_task.outputs['Y_val_output'],
+        # model_output=prepare_data_task.outputs['model_output']  # 確保從 prepare_data 中正確引用輸出
     )
     
-    evaluate_model_task = evaluate_model(
-        model_path = train_model_task.outputs['train_model_output'], 
-        x_test = prepare_data_task.outputs['x_test_output'], 
-        y_test = prepare_data_task.outputs['y_test_output']
-    )
-    
-    return evaluate_model_task.output
+    # 返回模型的準確度
+    return train_model_task.output  # 返回 train_model 任務的輸出
 
 if __name__ == '__main__':
-    kfp.compiler.Compiler().compile(diabetes_prediction_pipeline, 'diabetes_prediction_pipeline.yaml')
+    kfp.compiler.Compiler().compile(HeartDisease_prediction_pipeline, 'HeartDisease_prediction_pipeline.yaml')
